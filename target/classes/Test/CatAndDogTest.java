f5cd22af11072179c6068d9c04debabfff87a6bc
Ankitwadikhaye <87537059+Ankitwadikhaye@users.noreply.github.com>
2022-04-14 23:53:45 +0530

    Add files via upload

---
+++ target/classes/Test/CatAndDogTest.java
@@ -0,0 +1,34 @@
+package Test;
+
+import entityy.Cat;
+import entityy.Color;
+import entityy.Config;
+import entityy.Dog;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CatAndDogTest {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(Config.class);
+        /* CAT AND IT'S COLOR OBJECTS */
+        Cat c1= ctx.getBean(Cat.class);
+        Color color = new Color();
+        color.setBase_color("Blue");
+        color.setTexture_color("Blue");
+        c1.setCat_name("Bunny");
+        c1.setBreed_name("SyberianHusky");
+        c1.setOwner_name("Ankit");
+        c1.setColor(color);
+        c1.catDisplay();
+
+        /* DOG AND IT'S COLOR OBJECTS */
+        Dog d1=ctx.getBean(Dog.class);
+        Color color1 = new Color();
+        color1.setBase_color("Green");
+        color1.setTexture_color("Green");
+        d1.setDog_name("Bunnie");
+        d1.setBreed_name("AlaskanMalamute");
+        d1.setOwner_name("Ankit");
+        d1.setColor(color1);
+        d1.dogDisplay();
+    }
+}
